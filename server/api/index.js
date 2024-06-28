b4714ee71dc9d98b96cdc1b78cad6a970370c1cc
Mayur Bhoyar <mayurbhoyar4455@gmail.com>
2024-06-28 14:09:48 +0530

    corsissuefixed

--- server/api/index.js
+++ server/api/index.js
@@ -8,7 +8,14 @@ const authRoute = require("../routes/authRoute.js");
 const productRoute = require("../routes/productRoute.js");
 const bodyParser = require("body-parser");
 dotenv.config();
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://furnowebsite.vercel.app", // Replace with your frontend origin
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+  })
+);
+
 // Middleware to handle large payloads
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
